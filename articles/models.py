a093e0d3fdc778d1a77d4866203e8eba51aa05a7
Tatsuyoung <katoukumiko@Linus-no-iMac.local>
2019-07-23 14:18:03 +0900

    foreignkey change

--- articles/models.py
+++ articles/models.py
@@ -10,7 +10,7 @@ class Article(models.Model):
     body = models.TextField(blank=False)
     date = models.DateTimeField(default=timezone.now)
     thumb = models.ImageField(default='no-image.png', blank=True, upload_to='article_pics')
-    author = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     like = models.ManyToManyField(User, related_name="likes", blank=True)
 
     def __str__(self):
