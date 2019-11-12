2afd458c2175a5a90d5f086c58d07e6acbb2d06c
Tatsuyoung <katoukumiko@Linus-no-iMac.local>
2019-11-12 11:24:55 +0900

    add post count

--- articles/models.py
+++ articles/models.py
@@ -19,7 +19,7 @@ class Article(models.Model):
     like = models.ManyToManyField(User, related_name="likes", blank=True)
 
     def __str__(self):
-        return self.title
+        return '{} - {}'.format(str(self.title), str(self.author))
 
     def snippet(self):
         return self.body[:50] + '...'
