179acd4eee35f813ae3a62e0381447d3d5e4031b
Tatsuyoung <katoukumiko@Linus-no-iMac.local>
2019-12-05 09:49:07 +0900

    django update==2.2.8

--- assets/progress.js
+++ assets/progress.js
@@ -25,8 +25,7 @@
               contentType : false,
               success : function(data){
                   alert('Uploaded successful!!!');
-//                  location.href = 'http://127.0.0.1:8000';
-                  location.href = 'https://localthings.herokuapp.com'
+                  location.href = '/';
               }
           });
           return false;
