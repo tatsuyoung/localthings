bfd1b00c2d7bdd17e1ab1d7ab923c2212a49f6a3
Tatsuyoung <katoukumiko@Linus-no-iMac.local>
2020-04-21 11:18:12 +0900

    change admin header and title

--- articles/admin.py
+++ articles/admin.py
@@ -5,6 +5,8 @@ from .models import Article, Comment, Category
 class ArticleAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'date')
     readonly_fields = ['date']
+    search_fields = ['title', 'body']
+    list_filter = ['category']
 
 
 class CommentAdmin(admin.ModelAdmin):
--- trydjango/urls.py
+++ trydjango/urls.py
@@ -41,3 +41,7 @@ urlpatterns = [
 if settings.DEBUG:
     #urlpatterns += staticfiles_urlpatterns()
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+admin.site.site_header = "LOCAL THINGS ADMIN"
+admin.site.site_title = "LOCAL THINGS ADMIN"
