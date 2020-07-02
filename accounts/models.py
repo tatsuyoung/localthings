3c57df14ab970654cb4ec23e6b5b700c6d4b80d9
Tatsuyoung <katoukumiko@Linus-no-iMac.local>
2020-07-02 14:58:14 +0900

    add user background image

--- accounts/models.py
+++ accounts/models.py
@@ -25,6 +25,7 @@ class Profile(models.Model):
     image = models.ImageField(default='default.png',
                               upload_to='profile_pics',
                               help_text='著作権があるものはiconにはできません。')
+    bg = models.ImageField(upload_to='user_background', blank=True, null=True, help_text='著作権を確認してください。')
     bio = models.TextField(max_length=160, blank=True, null=True)
     website = models.URLField(max_length=250, blank=True, null=True)
     followers = models.ManyToManyField(User, related_name='is_following', blank=True)
@@ -41,6 +42,12 @@ class Profile(models.Model):
             this = Profile.objects.get(id=self.id)
             if this.image != self.image:
                 self.set_image()
+        if not self.id:
+            self.set_bg()
+        else:
+            this = Profile.objects.get(id=self.id)
+            if this.bg != self.bg:
+                self.set_bg()
         return super(Profile, self).save(*args, **kwargs)
 
     def set_image(self):
@@ -83,6 +90,49 @@ class Profile(models.Model):
                         pilImage.save(output, format='JPEG', quality=90)
                         output.seek(0)
                         self.image = File(output, self.image.name)
+        except(AttributeError, KeyError, IndexError):
+            pass
+
+    def set_bg(self):
+        try:
+            if self.bg:
+                pilImage = Img.open(BytesIO(self.bg.read()))
+                for orientation in ExifTags.TAGS.keys():
+                    if ExifTags.TAGS[orientation] == 'Orientation':
+                        break
+                e = pilImage._getexif()
+                if e is not None:
+                    exif = dict(e.items())
+                    if exif[orientation] == 3:
+                        pilImage = pilImage.rotate(180, expand=True)
+                    elif exif[orientation] == 6:
+                        pilImage = pilImage.rotate(270, expand=True)
+                    elif exif[orientation] == 8:
+                        pilImage = pilImage.rotate(90, expand=True)
+
+                    output = BytesIO()
+                    if pilImage.height > 400 or pilImage.width > 400:
+                        size = (400, 400)
+                        pilImage_fit = ImageOps.fit(pilImage, size, Img.ANTIALIAS)
+                        pilImage_fit.save(output, format='JPEG', quality=70)
+                        output.seek(0)
+                        self.bg = File(output, self.bg.name)
+                    else:
+                        pilImage.save(output, format='JPEG', quality=90)
+                        output.seek(0)
+                        self.bg = File(output, self.bg.name)
+                elif e is None:
+                    output = BytesIO()
+                    if pilImage.height > 200 or pilImage.width > 200:
+                        size = (200, 200)
+                        pilImage_fit = ImageOps.fit(pilImage, size, Img.ANTIALIAS)
+                        pilImage_fit.save(output, format='JPEG', quality=70)
+                        output.seek(0)
+                        self.bg = File(output, self.bg.name)
+                    else:
+                        pilImage.save(output, format='JPEG', quality=90)
+                        output.seek(0)
+                        self.bg = File(output, self.bg.name)
         except(AttributeError, KeyError, IndexError):
             pass
 
