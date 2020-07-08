2d5e54cfe8b5c474636610828245f9c9ab21e71a
Tatsuyoung <katoukumiko@Linus-no-iMac.local>
2020-07-08 16:55:29 +0900

    add sign up backends

--- accounts/views.py
+++ accounts/views.py
@@ -30,7 +30,7 @@ def signup_view(request):
         form = UserCreationForms(request.POST)
         if form.is_valid():
             user = form.save()
-            login(request, user)
+            login(request, user, backend='django.contrib.auth.backends.ModelBackend')
             return redirect('articles:list')
     else:
         form = UserCreationForms()
