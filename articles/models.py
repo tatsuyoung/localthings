0df6654e1a5fff7135aa83cc889fd82e72866146
Tatsuyoung <katoukumiko@Linus-no-iMac.local>
2020-07-30 15:28:46 +0900

    remove models import

--- articles/models.py
+++ articles/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.template.defaultfilters import slugify
 from django.urls import reverse
 from django.utils import timezone
 from PIL import ImageOps
