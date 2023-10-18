63525fc34e9fe2e8dab4d3284f80f6447da2a063
Linus <young@LinusnoMacBook-Pro.local>
2023-10-18 23:41:46 +0900

    urllib3 upgrade

--- assets/js/progress.js
+++ assets/js/progress.js
@@ -1,44 +1,37 @@
-function upload(event) {
-    event.preventDefault();
-    var formData = new FormData($('#form').get(0));
+console.log("hello Progress")
+
+function upload(e) {
+    e.preventDefault();
+    var formData = new FormData($("#form").get(0))
 
     $.ajax({
-        xhr : function(){
-        var xhr = new window.XMLHttpRequest();
-        xhr.upload.addEventListener('progress', function(e){
-            if (e.lengthComputable){
-                console.log('Bytes Loaded: ' + e.loaded);
-                console.log('Total Size: ' + e.total);
-                console.log('Percentage Uploaded: ' + (e.loaded / e.total))
-                var percent = Math.round((e.loaded / e.total) * 100);
-                $('.progress-bar').text(percent + '%');
-                $('.progress-bar').css('width', percent + '%');
+        xhr: function () {
+            const xhr = new window.XMLHttpRequest();
+            xhr.upload.addEventListener('progress', function (evt) {
+                if (evt.lengthComputable) {
+                    console.log('Bytes Loaded: ' + evt.loaded);
+                    console.log('Total Size  : ' + evt.total);
+                    console.log('Percentage Uploaded: ' + (evt.loaded / evt.total));
+                    const percent = Math.round((evt.loaded / evt.total) * 100);
+                    $("#progressBar").attr('aria-valuenow', percent).css('width', percent + '%').text(percent + '%');
                 }
-            });
+            }, false);
             return xhr;
         },
-        type : $(this).attr('method'),
-        url  : $(this).attr('action'),
-        data : formData,
+        type: $("#form").attr('method'),
+        url: $("#form").attr('action'),
+        data: formData,
         cache: false,
-        processData : false,
-        contentType : false,
-        success : function(data){
+        processData: false,
+        contentType: false,
+        success: function (response) {
             alert('Uploaded successful!!!');
             location.href = '/';
-        }
+        },
     });
     return false;
-    }
-    $(function() {
-        $('#form').submit(upload);
+}
+
+$(function () {
+    $("#form").submit(upload);
 });
-
-
-
-
-
-
-
-
-
