cb0ddaaa07bc30b7885a56f3842f8fdb68405021
tatsuyoung <kujira.gogo@gmail.com>
2024-09-04 11:04:47 +0900

    Django Update

--- trydjango/settings.py
+++ trydjango/settings.py
@@ -202,7 +202,8 @@ AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL       = None
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-
+# django 3.2.15
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 # Security
 
 # security.W004 DEBUG True off 86400=1day 31536000=365day
