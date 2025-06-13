f861a1c3a230fde5412dac00ff9abeb4d128ec9d
tatsuyoung <kujira.gogo@gmail.com>
2025-06-13 10:21:14 +0900

    add UFO animation

---
+++ assets/js/ufo.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function() {
+    function showUfo() {
+        const ufo = document.getElementById('ufo');
+        if (!ufo) return;
+        ufo.classList.remove('ufo');
+        // opacityで表示
+        ufo.style.opacity = '1';
+        // 再描画トリガー
+        void ufo.offsetWidth;
+        ufo.classList.add('ufo');
+        setTimeout(() => {
+            ufo.classList.remove('ufo');
+            ufo.style.opacity = '0';
+        }, 6000);
+    }
+    if (Math.random() < 0.8) {
+        setTimeout(showUfo, 1000 + Math.random() * 3000);
+    }
+});
