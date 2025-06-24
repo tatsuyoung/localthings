8d0211a308503a5a33ec2678b5f453f7b6758564
tatsuyoung <kujira.gogo@gmail.com>
2025-06-24 12:25:12 +0900

    UIの改善

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -1,21 +1,28 @@
 const nav = document.getElementById("nav");
-const scrollUp = "scroll-up";
+const scrollUp   = "scroll-up";
 const scrollDown = "scroll-down";
 let lastScroll = 0;
 
+// 初期状態を scroll-up に
+nav.classList.add(scrollUp);
+
 window.addEventListener("scroll", () => {
     const currentScroll = window.pageYOffset;
-    if (currentScroll == 0){
+    if (currentScroll === 0) {
         nav.classList.remove(scrollUp);
+        nav.classList.remove(scrollDown);
         return;
     }
 
-    if (currentScroll > lastScroll && !nav.classList.contains(scrollDown)){
+    if (currentScroll > lastScroll && !nav.classList.contains(scrollDown)) {
+        // 下にスクロール → ナビ非表示
         nav.classList.remove(scrollUp);
         nav.classList.add(scrollDown);
-    } else if (currentScroll < lastScroll && nav.classList.contains(scrollDown)){
+    } else if (currentScroll < lastScroll && nav.classList.contains(scrollDown)) {
+        // 上にスクロール → ナビ再表示
         nav.classList.remove(scrollDown);
         nav.classList.add(scrollUp);
     }
+
     lastScroll = currentScroll;
 });
